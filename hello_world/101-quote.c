9041244c7928bce8bc9a4a117dc2c02c5919a171
Maudev7 <mausac1501@gmail.com>
2022-10-04 07:56:06 -0700

    Task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print de phrase: and that piece of art is useful"
+ *  - Dora Korpar, 2015-10-19
+ *  
+ * Return: 1 if exited properly, followed by a new line,
+ * to the standard error.
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
