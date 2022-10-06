f315a4e5b1666ffb19e5caf0be71fe1ceae8400f
Maudev7 <mausac1501@gmail.com>
2022-10-06 13:49:02 -0700

    Task 6: Print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
