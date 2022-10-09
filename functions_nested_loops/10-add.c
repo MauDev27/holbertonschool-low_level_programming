96e0a197efd091c6fa7dce60074cae3d3ce17f91
Maudev7 <mausac1501@gmail.com>
2022-10-09 00:18:14 -0700

    Task 10: function that adds two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @a: number to the checked
+ * @b: number to the checked
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
