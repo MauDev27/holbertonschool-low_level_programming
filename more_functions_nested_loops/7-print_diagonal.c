5be8037746bf7f5ffd7def84916feb3397e082e2
Maudev7 <mausac1501@gmail.com>
2022-10-10 03:12:09 -0700

    Task 7: solved

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: the parameter to be used
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int a, b; 
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++ )
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
