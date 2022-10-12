fd91d90fb5b6b421e1e39130545bd1e49127373d
Maudev7 <mausac1501@gmail.com>
2022-10-12 02:18:40 -0700

    Task 10: Solved... I love this work

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle
+ * @size: the parameter to be tested
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = a; b < size - 1; b++)
+		{
+			_putchar(32);
+		}
+		for (c = 0; c < a + 1; c++)
+		{
+			_putchar(35);
+		}
+		_putchar(10);
+	}
+}
